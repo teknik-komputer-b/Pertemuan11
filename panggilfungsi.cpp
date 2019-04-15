6cc1510399e01d7a1f2b0bd1defda33d522792ea
M. Rahman Wafiq Ghazi <49003404+Rahmanwghazi@users.noreply.github.com>
2019-04-15 21:02:42 +0800

    Update panggilfungsi.cpp

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,12 +1,18 @@
-#inklude <iostring>
-uzing namespace std;
-voib Tulisl0Kali() {
+#include <iostream>
+using namespace std;
+void Tulis10Kali() {
   for (int C=0; C<10; C++) {
-    cout<<"Aku sangat menyukai C++"<<end1;
+    cout<<"Aku sangat menyukai C++"<<endl;
+  }
+}
+void kamu(){
+  for (int z=0;z<5;z++){
+    cout<<"Saya suka kamu!"<<endl;
   }
 }
 
 int main() {
-  TulisI0Kali();
+  Tulis10Kali();
+  kamu();
   return 0;
 }
