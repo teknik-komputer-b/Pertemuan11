57aa685934e47f9d3c963579fd1c6fd44d63f320
nurinayahyusuf <48977773+nurinayahyusuf@users.noreply.github.com>
2019-04-15 21:00:48 +0800

    Update panggilfungsi.cpp
    menambahkan fungsi kamu dan memperbaiki ejaan kata yang salah

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
+void Tulis10Kali () {
+  for (int C=0; C<10; C++) {
+    cout<<"Aku sangat menyukai C++"<<endl;
+  }
+}
 void kamu () {
   for (int C=0; C<=4; C++) {
     cout<<"Saya suka kamu!"<<endl;
   }
 }
-
 int main() {
+  Tulis10Kali ();
+  cout<<endl;
   kamu ();
   return 0;
 }
