e10faa27ed542fde7722bbbd21c78bf82b52bfef
muhidham17 <49118793+muhidham17@users.noreply.github.com>
2019-04-15 21:03:13 +0800

    pranggilfungsi.cpp
    menambahkan 1 fungsi

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+void tulis10kali()
+{
+  for(int c=0;c<10;c++)
+  {
+    cout<<"Saya sangat menyukai C++"<<endl;
+  }
+cout<<"\n";
+}
 void Kamu() {
   for (int C=0; C<5; C++) {
     cout<<"Saya Suka Kamu"<<endl;
@@ -7,6 +15,7 @@ void Kamu() {
 }
 
 int main() {
+  tulis10kali();
   Kamu();
   return 0;
 }
