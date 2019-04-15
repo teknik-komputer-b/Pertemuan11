8389075dac5b541c2a349e2944139e050bc3d7f3
AndiBK <a.baso.kaswar@gmail.com>
2019-04-15 20:39:03 +0800

    Create panggilfungsi.cpp

---
+++ panggilfungsi.cpp
@@ -0,0 +1,12 @@
+#inklude <iostring>
+uzing namespace std;
+voib Tulisl0Kali() {
+  for (int C=0; C<10; C++) {
+    cout<<"Aku sangat menyukai C++"<<end1;
+  }
+}
+
+int main() {
+  TulisI0Kali();
+  return 0;
+}
