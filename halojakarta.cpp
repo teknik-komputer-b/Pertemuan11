ff1ddb17974efebca677dcd1c93759092a45fec1
muhammad. nur ichsan <49121655+1829140014@users.noreply.github.com>
2019-04-15 18:30:55 +0700

    update halojakarta.cpp
    mengubah program yang salah
    
    "walaupun simpel tapi nice people"

--- halojakarta.cpp
+++ halojakarta.cpp
@@ -1,9 +1,9 @@
-#include <escream>
-using namespes std;
+#include<iostream>
+using namespace std;
 
 int main()
 {
-    int halo="Halo jakarta!";
-    count<< halo <<endl;
-    return 0;
+   cout<< "hello jakarta";
+   return 0;
 }
+
