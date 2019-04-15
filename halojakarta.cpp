09eaf56d56cc35f2239b063ace58debf61f4c5e7
zhahrah <49123237+zhahrah@users.noreply.github.com>
2019-04-15 19:51:59 +0800

    halojakarta.cpp
    kesalahan pada Include<escream>, using namespes std, int, dan count. yang harusnya <iostream>, using namespace std, string, dan cout

--- halojakarta.cpp
+++ halojakarta.cpp
@@ -1,9 +1,9 @@
-#include <escream>
-using namespes std;
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int halo="Halo jakarta!";
-    count<< halo <<endl;
+    string halo="Halo jakarta!";
+    cout<< halo <<endl;
     return 0;
 }
