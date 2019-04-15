1d73503f5f16e7246da58a0cac5e97a80fb2fc35
Nur Rahmi <49005228+Nurrahmi09@users.noreply.github.com>
2019-04-15 20:59:50 +0800

    Update panggilfungsi.cpp

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 void Tulisl0Kali() {
-  for (int C=0; C<5; C++) {
-    cout<<"saya suka kamu!"<<endl;
+  for (int C=0; C<10; C++) {
+    cout<<"aku sangat menyukai C++!"<<endl;
   }
+}
+void kamu() {
+   for (int C=0; C<5; C++) {
+     cout<< "saya suka kamu!"<<endl;
+   }
 }
 
 int main() {
   Tulisl0Kali();
+  kamu();
   return 0;
 }
