b710cdbe0ad0903d635c184fd6a99a08fa80fa5b
zhahrah <49123237+zhahrah@users.noreply.github.com>
2019-04-15 21:05:36 +0800

    panggilfungsi.cpp
    kata programnya ada yang kurang tepat, penambahan 1 fungsi

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,12 +1,18 @@
-#inklude <iostring>
-uzing namespace std;
-voib Tulisl0Kali() {
-  for (int C=0; C<10; C++) {
-    cout<<"Aku sangat menyukai C++"<<end1;
+#include <iostream>
+using namespace std;
+void tulis10kali() {
+  for(int c=0; c<10; c++) {
+    cout<<"Saya sangat menyukai C++"<<endl;
+  }
+}
+void kamu() {
+  for (int C=0; C<5; C++) {
+    cout<<"Aku suka Kamu"<<endl;
   }
 }
 
 int main() {
-  TulisI0Kali();
+  tulis10kali();
+  kamu();
   return 0;
 }
