3d32b6fdf1959f2bc71f03bf8bc5d18436d77afe
muhammadekafirdaus <49117901+muhammadekafirdaus@users.noreply.github.com>
2019-04-15 20:04:01 +0700

    Update panggilfungsi.cpp
    Syntax salah , dan ini update-an terbaru yang pertama saya salah pak sekarang saya sudah menambahkan 1 fungsi panggilan baru

--- panggilfungsi.cpp
+++ panggilfungsi.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 void Tulisl0Kali() {
-  for (int C=0; C<5; C++) {
-    cout<<"saya suka kamu!"<<endl;
+  for (int C=0; C<10; C++) {
+    cout<<"Aku sangat menyukai C++"<<endl;
   }
 }
-
+void Tulis5Kali() {
+  for (int C=0; C<5; C++) {
+    cout<<"Aku suka kamu!"<<endl;
+  }
+}
 int main() {
   Tulisl0Kali();
+  Tulis5Kali();
   return 0;
 }
-
