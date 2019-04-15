2b344bc22bae67754f33541c424051f2f483540c
muhidham17 <49118793+muhidham17@users.noreply.github.com>
2019-04-15 20:20:08 +0800

    cetakmantan.cpp
    agar barisnya berkurang tapi tampilan tetap sama kita masukkan fungsi for kedalam programnya

--- cetakmantan.cpp
+++ cetakmantan.cpp
@@ -2,15 +2,10 @@
 using namespace std;
 int main()
 {
+ int a;
+ for (a=1;a<=10;a++)
+ {
  cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- return 0;
+ }
+  return 0;
 }
