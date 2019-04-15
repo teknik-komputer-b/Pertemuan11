9dbdec86f85998261c73ea5d2126d7339ba9fe8d
AndiBK <a.baso.kaswar@gmail.com>
2019-04-15 20:04:56 +0800

    Create cetakmantan.cpp

---
+++ cetakmantan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ cout<<"Hello Mantan!"<<endl;
+ return 0;
+}
