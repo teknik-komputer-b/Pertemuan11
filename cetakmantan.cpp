c4036463f15605f7fbce05ce4dbcf1002d6f4430
Nur Fadillah Bustamin <48977915+FadillahBustamin@users.noreply.github.com>
2019-04-15 19:09:50 +0700

    Update cetakmantan.cpp

--- cetakmantan.cpp
+++ cetakmantan.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 int main()
 {
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
- cout<<"Hello Mantan!"<<endl;
+ for(int i=0; i<10; i++){
+    cout<<"Hello Mantan!"<<endl;
+ }
  return 0;
 }
